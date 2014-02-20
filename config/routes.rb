6ae6956a541fb1b2a1e1733ffd76948125f16a64
Mario Andrés Correa <admin@macool.me>
2014-02-20 13:10:23 -0500

    added AdminConstraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
+require "admin_constraint"
+
 SamsaWebsite::Application.routes.draw do
   match "/auth/:provider/callback", to: "sessions#create", via: [:get, :post]
   get "/logout", to: "sessions#destroy", as: :logout
+
+  get :admin, to: "admin#index", constraints: AdminConstraint.new
+
+  namespace :admin do
+  end
+
+  root "home#index"
 
   get "*a", to: "application#routing_error"
 end
