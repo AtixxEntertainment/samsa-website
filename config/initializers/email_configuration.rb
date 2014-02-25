c4c2ddaafd217000fd47b69b7a8e5e29bd19e73d
Mario Andrés Correa <admin@macool.me>
2014-02-25 15:31:07 -0500

    using environment variables to configure mailer

--- config/initializers/email_configuration.rb
+++ config/initializers/email_configuration.rb
@@ -1,1 +1,8 @@
-ActionMailer::Base.smtp_settings = YAML::load_file(File.join(Rails.root.to_s, "config", "email.yml")).symbolize_keys
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "shiriculapo.com",
+  user_name: ENV["SMTP_USER"],
+  password: ENV["SMTP_PASSWORD"],
+  authentication: "plain"
+}
