5be2ed7ce40b780bb2cdc13dcbfdbb0e30c7d14f
Mario Andrés Correa <admin@macool.me>
2014-02-25 15:02:40 -0500

    added default character's seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220032219) do
+ActiveRecord::Schema.define(version: 20140225143117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "header_characters", force: true do |t|
+    t.string   "nombre",                          null: false
+    t.integer  "profundidad",         default: 1
+    t.integer  "posicion_horizontal", default: 0
+    t.integer  "posicion_vertical",   default: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "profiles", force: true do |t|
     t.integer  "user_id",      null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Rails.logger.debug "Creating default header characters.."
+
+[ {nombre: "gimb", profundidad: 5, posicion_horizontal: 7, posicion_vertical: -73},
+  {nombre: "camila", profundidad: 4, posicion_horizontal: 15, posicion_vertical: -42},
+  {nombre: "hector", profundidad: 3, posicion_horizontal: 25, posicion_vertical: -41},
+  {nombre: "lina", profundidad: 2, posicion_horizontal: 37, posicion_vertical: -150},
+  {nombre: "elder", profundidad: 6, posicion_horizontal: 33, posicion_vertical: 41},
+  {nombre: "robin", profundidad: 5, posicion_horizontal: 40, posicion_vertical: -44},
+  {nombre: "destructor", profundidad: 6, posicion_horizontal: 68, posicion_vertical: 103},
+  {nombre: "sonia", profundidad: 3, posicion_horizontal: 81, posicion_vertical: -92},
+  {nombre: "boris", profundidad: 4, posicion_horizontal: 95, posicion_vertical: -22},
+  {nombre: "demi", profundidad: 4, posicion_horizontal: 75, posicion_vertical: -30},
+  {nombre: "samsa", profundidad: 1, posicion_horizontal: 58, posicion_vertical: 0}].each do |character|
+  HeaderCharacter.create character
+end
