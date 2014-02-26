ae11bf477cd475ed1fee758a8c3a4d8439ef061e
Mario Andrés Correa <admin@macool.me>
2014-02-26 11:14:50 -0500

    redirecting admins to admin panel after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    session[:user_id] = User.create_or_update_from_omniauth(request.env["omniauth.auth"]).id
-    redirect_to root_path
+    user = User.create_or_update_from_omniauth(request.env["omniauth.auth"])
+    session[:user_id] = user.id
+    if user.admin?
+      redirect_to admin_path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
