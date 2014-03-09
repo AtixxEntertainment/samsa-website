78361b72a892a07418406f29a07656983c9031d9
Mario Andrés Correa <a@macool.me>
2014-03-09 10:41:32 -0500

    CommentsController now handles not-authenticated users' comments
    This is only a bootstrap

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_filter :require_login, only: :create
+
   expose(:post) { Post.cached_find params[:post_id] }
   expose :comment, attributes: :comment_params
 
@@ -22,5 +24,21 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit :body
+  end
+
+  def require_login
+    unless current_user
+      session[:comment_body] = comment_params[:body]
+      flash[:warning] = "Debes autenticarte"
+      respond_to do |format|
+        format.html {
+          redirect_to login_path
+        }
+        format.js {
+          render :require_login
+        }
+      end
+      false
+    end
   end
 end
