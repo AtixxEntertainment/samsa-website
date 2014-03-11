abe989596754dbdf09ae20e4fd19fd8ce5dbbbc5
Mario Andrés Correa <a@macool.me>
2014-03-11 09:45:40 -0500

    added vote methods to Comment and sorting comments by reputation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   expose_decorated(:post) { Post.landing }
-  expose_decorated(:comments) { post.comments.includes(:user) }
-  expose :comment
+  expose_decorated(:comments) {
+    post.comments.includes(:user).by_votes
+  }
+  expose(:comment) { Comment.new }
 
   def index
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+# scopes
+  scope :by_votes, -> {
+    # find_with_reputation(:votes, :all, sort: "votes desc")   # doesn't work with rails 4
+    all.sort { |x,y| y.votes <=> x.votes }
+  }
+
 # validations
   validates :user_id, :post_id, :body, presence: true
   validate :validate_user_allowed_to_comment
@@ -13,5 +19,9 @@ class Comment < ActiveRecord::Base
 # methods
   def validate_user_allowed_to_comment
     errors.add(:base, "No puedes agregar comentarios") if user.banned?
+  end
+
+  def votes
+    reputation_for(:votes).to_i
   end
 end
