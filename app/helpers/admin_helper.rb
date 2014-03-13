f35349ce9dcc0802900712dcc39b467037d87483
Mario Andrés Correa <a@macool.me>
2014-03-13 12:58:34 -0500

    AdminHelper#navbar_item is only used on admin

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,13 @@
+module AdminHelper
+  def navbar_item(title, link, options={})
+    opts = if options[:tab]
+      { data: { toggle: "tab" } }
+    else
+      nil
+    end
+
+    content_tag :li, class: "#{"active" if @active_item == title.downcase.to_sym}" do
+      link_to title, link, opts
+    end
+  end
+end
