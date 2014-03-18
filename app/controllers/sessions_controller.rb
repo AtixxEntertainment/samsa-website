9823b7bf5ee2317d576bf54bc885a9ca05084f28
Mario Andrés <a@macool.me>
2014-03-18 17:21:33 -0500

    bugfix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    user = User.create_or_update_from_omniauth(request.env["omniauth.auth"])
-    session[:user_id] = user.id
+    @user = User.create_or_update_from_omniauth(request.env["omniauth.auth"])
+    session[:user_id] = @user.id
     redirect_to redirection_path
   end
 
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   def redirection_path
     if session[:attempt_comment]
       post_path session[:attempt_comment][:post_id], anchor: "comentar"
-    elsif user.admin?
+    elsif @user.admin?
       admin_path
     else
       root_path
