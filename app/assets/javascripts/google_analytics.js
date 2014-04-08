2c13055ab77d47a4dee8f95f6fd5a5252b15c9de
Mario Andrés Correa <a@macool.me>
2014-04-08 14:21:45 -0500

    update Google analytics key

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-49801920-1', 'samsarpg.com');
+ga('create', 'UA-49838723-1', 'samsarpg.com');
 ga('send', 'pageview');
