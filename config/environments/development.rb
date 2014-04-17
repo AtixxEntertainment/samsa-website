9b26d0f5be10e51150d4fbda7b1794eb0c0ce9ed
Mario Andrés Correa <a@macool.me>
2014-04-17 12:02:44 -0500

    use mailcatcher on development
    http://mailcatcher.me/

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ SamsaWebsite::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # mailcatcher http://mailcatcher.me/
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
