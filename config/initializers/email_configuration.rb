63c5fbab84d1df364f15bad3575ac5c2970780fd
Mario Andrés Correa <a@macool.me>
2014-04-17 14:28:45 -0500

    use mandrill to deliver emails

--- config/initializers/email_configuration.rb
+++ config/initializers/email_configuration.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.mandrillapp.com",
   port: 587,
-  domain: "shiriculapo.com",
+  domain: ENV["SMTP_DOMAIN"] || "shiriculapo.com",
   user_name: ENV["SMTP_USER"],
   password: ENV["SMTP_PASSWORD"],
   authentication: "plain"
