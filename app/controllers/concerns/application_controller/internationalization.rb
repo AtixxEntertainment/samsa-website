edef123fe3cdf443f2c2941d75fca86e25272f52
Mario Andrés Correa <a@macool.me>
2014-06-18 12:39:40 -0500

    add ApplicationController::Internationalization

---
+++ app/controllers/concerns/application_controller/internationalization.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  module Internationalization
+    extend ActiveSupport::Concern
+
+    included do
+      before_filter :set_locale
+      helper_method :current_locale
+    end
+
+    private
+
+    def set_locale
+      session[:current_locale] = params[:lang] if params[:lang].present?
+      I18n.locale = current_locale
+    end
+
+    def current_locale
+      session[:current_locale] or
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   def posts_in_navbar
     @posts_in_navbar ||= Post.in_navbar
   end
+
+  def link_to_locale(locale)
+    content_tag :span, class: "locale-option" do
+      link_to_unless current_locale_is?(locale), t("ui.locale.#{locale}"), lang: locale
+    end
+  end
+
+  def current_locale_is?(locale)
+    current_locale == locale
+  end
 end
