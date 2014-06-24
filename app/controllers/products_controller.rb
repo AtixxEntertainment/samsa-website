5db3e7777ab9a849fec939497aa9a1ceeffb7ad2
Mario Andrés Correa <a@macool.me>
2014-06-24 15:05:02 -0500

    allow comments inside Otros Productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
+  include Commentable
+
+  expose :page
   expose_decorated(:products) {
     Product.all.with_translations I18n.locale
   }
+
+  alias_method :commentable, :page
+
+  def index
+    self.page = Page.otros_productos
+  end
 end
