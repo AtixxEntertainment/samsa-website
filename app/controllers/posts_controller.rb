097fbcbd324d745f50ee849c9a52b37c0a4c6adb
Mario Andrés Correa <a@macool.me>
2014-06-24 14:39:03 -0500

    wtf?
    I don’t know what these are

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,18 +4,4 @@ class PostsController < ApplicationController
   expose_decorated :post
 
   alias_method :commentable, :post
-
-  def like
-    vote! 1
-  end
-
-  def dislike
-    vote! -1
-  end
-
-  private
-
-  def vote!(value)
-    post.add_or_update_evaluation(:votes, value, current_user)
-  end
 end
