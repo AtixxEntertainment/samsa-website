5d52358ce6ee5fe854dd1a6483da2bf9d5cd6b62
Mario Andrés Correa <a@macool.me>
2014-07-19 12:13:20 -0500

    make description longer

---
+++ db/migrate/20140719171116_make_description_bigger_in_profile.rb
@@ -0,0 +1,9 @@
+class MakeDescriptionBiggerInProfile < ActiveRecord::Migration
+  def up
+    change_column :profiles, :description, :text
+  end
+
+  def down
+    change_column :profiles, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624000243) do
+ActiveRecord::Schema.define(version: 20140719171116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,7 +124,7 @@ ActiveRecord::Schema.define(version: 20140624000243) do
     t.string   "email"
     t.string   "image"
     t.string   "location"
-    t.string   "description"
+    t.text     "description"
     t.string   "token_access"
     t.string   "token_secret"
     t.text     "raw_info"
