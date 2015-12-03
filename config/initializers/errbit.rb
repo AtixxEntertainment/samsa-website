6bfc6a9a6662c05fb850ab9a1a4f5cfd377ff459
Mario Andrés <a@macool.me>
2015-12-03 14:05:09 -0500

    Update errbit.rb

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = '11900e80d5acfd066b7da95ea07c624d'
+  config.api_key = '7ee052cbc5c53453ee89266bed17a3ca'
   config.host    = 'pangi.shiriculapo.com'
   config.port    = 80
   config.secure  = config.port == 443
